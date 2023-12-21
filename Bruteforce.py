53a0061b0dd17d962aa7caf278c667c3f84bfb86
A-khalid <82731518+pixelodin@users.noreply.github.com>
2023-12-21 10:11:41 +0000

    Intreduced Tried Combinations Set-Periodic Progress Bar Update-Execution Time Measurement
        Tried Combinations Set: Introduced a tried_combinations set to store combinations that have already been tried. This helps avoid unnecessary retries, speeding up the process.
    
        Periodic Progress Bar Update: The progress_bar is updated periodically rather than with every iteration. This reduces the number of console writes, which can be more efficient.
    
        Execution Time Measurement: Added a timer to measure and print the execution time of the script.
    
    These changes are aimed at improving the efficiency and speed of the script. The use of a set for tried combinations ensures that the script doesn't waste time retrying combinations it has already tested. The periodic update of the progress bar reduces the overhead of frequent console writes. Finally, the execution time measurement provides insight into the overall runtime of the script.

--- Bruteforce.py
+++ Bruteforce.py
@@ -1,5 +1,6 @@
 import subprocess
 import itertools
+import time
 
 def progress_bar(device, key, progress, length):
     print(f"\rKey: {key} [{'#' * progress}{' ' * (length - progress)}] {device}", end='', flush=True)
@@ -29,22 +30,30 @@ def main():
     devfile = f"./{device}.dat"
     charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     min_length = 1
-    max_length = 30  # Adjust the max length as needed
+    max_length = 8
     value = ""
+
+    tried_combinations = set()
 
     if not is_unlock_allowed(device):
         for length in range(min_length, max_length + 1):
             combinations = itertools.product(charset, repeat=length)
             for combination in combinations:
                 value = ''.join(combination)
-                progress_bar(device, "Searching for unlock code", 0, 100)  # Reset progress bar
-                if is_unlock_allowed(device):
+                progress_bar(device, "Searching for unlock code", 0, 100)
+
+                if value not in tried_combinations and is_unlock_allowed(device):
                     output = subprocess.run(["fastboot", "oem", "unlock", value], capture_output=True, text=True)
+
                     if "FAILED" not in output.stdout:
                         print(f"\nYour unlock code is: {value}")
                         print(f"Saving unlock code to {devfile}...")
                         save_progress(device, value)
                         return  # Exit if unlock code is found
 
+                    tried_combinations.add(value)
+
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    print(f"\nExecution time: {time.time() - start_time} seconds")
